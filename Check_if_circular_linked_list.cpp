11167c819a509a477559a6aa909c94323725369e
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-10-27 08:13:47 +0530

    Add files via upload

---
+++ Check_if_circular_linked_list.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+
+// } Driver Code Ends
+/* Link list Node
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+/* Should return true if linked list is circular, else false */
+class Solution {
+  public:
+    bool isCircular(Node *head) {
+        // Your code here
+        map<Node*, int> mp;
+        int count = 0;
+        Node *temp = head;
+        while(temp!=NULL){
+            if(mp.find(temp)==mp.end()){
+                mp[temp] = count;
+            }
+            else{
+                if(mp[temp]==0){
+                    return 1;
+                }
+                break;
+            }
+            count++;
+            temp = temp->next;
+        }
+        if(count == 0){
+            return 1;
+        }
+        
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above function
+int main() {
+    int t, k;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        cin >> k;
+        cin.ignore(); // Ignore the newline character after k
+
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        if (ss >> x) {
+            head = new Node(x);
+            tail = head;
+
+            while (ss >> x) {
+                tail->next = new Node(x);
+                tail = tail->next;
+            }
+
+            // Link the last node to the head if k is 1 to make it circular
+            if (k == 1) {
+                tail->next = head;
+            }
+        }
+
+        Solution obj;
+        cout << (obj.isCircular(head) ? "true" : "false") << endl;
+        cout << '~' << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
